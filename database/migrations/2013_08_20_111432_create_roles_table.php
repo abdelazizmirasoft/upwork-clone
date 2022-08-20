9103c7c5a6b9955f27c0873fcea809cab51147e9
abdelazizmirasoft <abdelaziz.mirasoft@gmail.com>
2022-08-20 20:16:37 +0100

    add migrations, setup custom login/register with fortify

--- database/migrations/2013_08_20_111432_create_roles_table.php
+++ database/migrations/2013_08_20_111432_create_roles_table.php
@@ -13,14 +13,9 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('roles', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->foreignId('role_id')->constrained();
-            $table->rememberToken();
             $table->timestamps();
         });
     }
@@ -32,6 +27,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('roles');
     }
 };
